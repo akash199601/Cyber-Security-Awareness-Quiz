50d7d8158613a45483745e06804e83262fa5e33f
sachinsrivas786 <53460336+sachinsrivas786@users.noreply.github.com>
2025-04-17 17:56:35 +0530

    Delete Sonata_Cyber_Quiz directory

--- Sonata_Cyber_Quiz/Quiz/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Candidate, Question, Option, QuizResult
-
-admin.site.register(Candidate)
-admin.site.register(Question)
-admin.site.register(Option)
-admin.site.register(QuizResult)
